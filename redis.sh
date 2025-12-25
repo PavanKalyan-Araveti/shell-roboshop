38b527508a4ba04b807ed8283c26bcf2e2e88aa5
Pavan <pavankalyan.Araveti1999@gmail.com>
2025-12-25 11:39:23 +0530

    redis server

--- redis.sh
+++ redis.sh
@@ -29,17 +29,17 @@ VALIDATE(){
 }
 
 dnf module disable redis -y &>>$LOG_FILE
-VALIDATE() $? "Disabling redis"
+VALIDATE $? "Disabling redis"
 dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE() $? "Enabling redis"
+VALIDATE $? "Enabling redis"
 dnf install redis -y &>>$LOG_FILE
-VALIDATE() $? "Installing redis"
+VALIDATE $? "Installing redis"
 sed -i -e '/s/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
-VALIDATE() $? "Allowing remote connection to redis"
+VALIDATE $? "Allowing remote connection to redis"
 systemctl enable redis &>>$LOG_FILE
-VALIDATE() $? "Enabling redis"
+VALIDATE $? "Enabling redis"
 systemctl start redis &>>$LOG_FILE
-VALIDATE() $? "Starting redis"
+VALIDATE $? "Starting redis"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
