a4f9eb303718e5f7f1b1d930c7f52cc45dd30bdf
Pavan <pavankalyan.Araveti1999@gmail.com>
2025-12-25 06:39:56 +0530

    AWS Config test

--- roboshop.sh
+++ roboshop.sh
@@ -5,12 +5,12 @@ SG_ID="sg-0d9388090da2dfd31"
 
 for instance in $@
 do
-   Instance_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output $instance)
+   Instance_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
    if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids i-0908b777cb346a92b --query 'Reservations[0].Instances[0].PrivateIpAddress' --output $instance)
+        IP=$(aws ec2 describe-instances --instance-ids i-0908b777cb346a92b --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
    else
-        IP=$(aws ec2 describe-instances --instance-ids i-0908b777cb346a92b --query 'Reservations[0].Instances[0].PublicIpAddress' --output $instance)
+        IP=$(aws ec2 describe-instances --instance-ids i-0908b777cb346a92b --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
    fi
 
    echo "$instance: $IP"
