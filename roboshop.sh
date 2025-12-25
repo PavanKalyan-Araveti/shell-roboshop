be27755f402bcae29428608dd4e5c1a0bcbbc21d
Pavan <pavankalyan.Araveti1999@gmail.com>
2025-12-25 06:34:39 +0530

    AWS Config test

---
+++ roboshop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0d9388090da2dfd31"
+
+for instance in $@
+do
+   Instance_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output tex)
+
+   if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids i-0908b777cb346a92b --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+   else
+        IP=$(aws ec2 describe-instances --instance-ids i-0908b777cb346a92b --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+   fi
+
+   echo "$instance: $IP"
+done
