3226cdc10ac1a6a47c63122c271a68158cb5213c
Pavan <pavankalyan.Araveti1999@gmail.com>
2025-12-25 07:41:37 +0530

    AWS Config test

---
+++ mongodb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$LOG_FILE.log"
+echo "Scripting started executing at : $(date)"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: Please run this script with root privilage"
+    exit -1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 is $R Failure $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is $G success $N" | tee -a $LOG_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing Mongodb"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable Mongodb"
+
+systemctl start mongod
+VALIDATE $? "Start Mongodb"
